9b6bcb35c005a57cb4abbfe0f066613f416516cb
Ryan McClure <rmcclure@reachify.io>
2018-02-18 12:50:58 -0800

    fix name, add custom catch block

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var babylon = require('babylon');
  */
 var getCatchBlock = function (functionName) {
   return `
-    var Err = require('babel-plugin-async-errors/src/Err.js');
+    var Err = require('babel-plugin-pretty-path-errors/src/Err.js');
     var path = '${functionName}';
 
     if (err.name === 'PrettyPathError') {
@@ -27,25 +27,70 @@ module.exports = function ({ types: t }) {
       Program(programPath) {
         programPath.traverse({
           Function: function (path) {
-            // Ignore if no function name
-            if (!path.node.id) return;
-
-            // Ignore if try statement already exists
+            // Ignore if not a function or no function name exists
             if (
-              path.node.body &&
-              path.node.body.body &&
-              t.isTryStatement(path.node.body.body[0])
+              path.node.type !== 'FunctionDeclaration' ||
+              !path.node.id
             ) return;
 
+            var body;
+
+            if (path.node.body && path.node.body.body) {
+              body = path.node.body.body;
+            }
+
+            // Ignore if try statement already exists
+            var hasTryStatement = body && body.find(function(body) {
+              return t.isTryStatement(body);
+            });
+
+            if (hasTryStatement) return;
+
             var name = path.node.id.name;
+            var catchBlockIndex = -1;
+
+            // If @onError exists in main block, move code to catch block
+            if (body) {
+              catchBlockIndex = body.findIndex(function(body, index) {
+                if (
+                  body.leadingComments &&
+                  body.leadingComments.length &&
+                  body.leadingComments[0].value.match(/@onError/g)
+                ) {
+                  return true;
+                }
+                return false;
+              });
+            }
+
+            var tryBody = path.node.body;
+            var catchBody = [];
+
+            // Separate custom catch body from try body
+            if (catchBlockIndex > -1) {
+              tryBody.body = body.slice(0, catchBlockIndex);
+              catchBody = body.slice(catchBlockIndex);
+
+              // Remove @onError leading comment from catch body
+              delete catchBody[0].leadingComments;
+
+              // Remove trailing comments from preceeding block
+              if (catchBlockIndex > 0) {
+                delete tryBody.body[catchBlockIndex - 1].trailingComments;
+              }
+            }
+
+            catchBody = catchBody.concat(
+              babylon.parse(getCatchBlock(name)).program.body
+            );
 
             path.node.body = t.blockStatement([
               t.tryStatement(
-                path.node.body,
+                tryBody,
                 t.catchClause(
                   t.identifier('err'),
                   t.blockStatement(
-                    babylon.parse(getCatchBlock(name)).program.body
+                    catchBody
                   )
                 )
               )
