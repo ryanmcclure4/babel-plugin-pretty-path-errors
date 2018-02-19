a2f62e502b17fe371f501d25c73b93c406d6067d
Ryan McClure <rmcclure@reachify.io>
2018-02-18 18:21:27 -0800

    add parent name in case of class

--- src/index.js
+++ src/index.js
@@ -9,11 +9,71 @@ var getCatchBlock = function (functionName) {
     var path = '${functionName}';
 
     if (err.name === 'PrettyPathError') {
-      path = err.functionPath + ' -> ' + path;
+      path = path + ' -> ' + err.functionPath;
     }
 
     throw new Err(err, path);
   `;
+};
+
+var insertTryStatement = function ({ path, body, name, t }) {
+  var catchBlockIndex = -1;
+  
+  // If @onError exists in main block, move code to catch block
+  if (body) {
+    catchBlockIndex = body.findIndex(function(body, index) {
+      if (
+        body.leadingComments &&
+        body.leadingComments.length &&
+        body.leadingComments[0].value.match(/@onError/g)
+      ) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  var tryBody = path.node.body;
+  var catchBody = [];
+
+  // Separate custom catch body from try body
+  if (catchBlockIndex > -1) {
+    tryBody.body = body.slice(0, catchBlockIndex);
+    catchBody = body.slice(catchBlockIndex);
+
+    // Remove @onError leading comment from catch body
+    delete catchBody[0].leadingComments;
+
+    // Remove trailing comments from preceeding block
+    if (catchBlockIndex > 0) {
+      delete tryBody.body[catchBlockIndex - 1].trailingComments;
+    }
+  }
+
+  catchBody = catchBody.concat(
+    babylon.parse(getCatchBlock(name)).program.body
+  );
+
+  path.node.body = t.blockStatement([
+    t.tryStatement(
+      tryBody,
+      t.catchClause(
+        t.identifier('err'),
+        t.blockStatement(
+          catchBody
+        )
+      )
+    )
+  ]);
+};
+
+var modifyTryStatement = function ({ path, body, name, existingTryStatement, t }) {
+  const handlerBody = existingTryStatement.handler.body.body;
+  const catchBody = handlerBody.concat(
+    babylon.parse(getCatchBlock(name)).program.body
+  );
+
+  existingTryStatement.handler.body = t.blockStatement(catchBody);
 };
 
 module.exports = function ({ types: t }) {
@@ -43,66 +103,24 @@ module.exports = function ({ types: t }) {
 
             if (path.node.id) name = path.node.id.name;
             if (path.node.key) name = path.node.key.name;
+            if (path.node.type === 'ClassMethod') {
+              name = path.parentPath.container.id.name + ' -> ' + name;
+            }
 
             if (path.node.body && path.node.body.body) {
               body = path.node.body.body;
             }
 
             // Ignore if try statement already exists
-            var hasTryStatement = body && body.find(function(body) {
+            var existingTryStatement = body.find(function(body) {
               return t.isTryStatement(body);
             });
 
-            if (hasTryStatement) return;
-
-            var catchBlockIndex = -1;
-
-            // If @onError exists in main block, move code to catch block
-            if (body) {
-              catchBlockIndex = body.findIndex(function(body, index) {
-                if (
-                  body.leadingComments &&
-                  body.leadingComments.length &&
-                  body.leadingComments[0].value.match(/@onError/g)
-                ) {
-                  return true;
-                }
-                return false;
-              });
+            if (existingTryStatement) {
+              modifyTryStatement({ path, name, body, existingTryStatement, t })
+            } else {
+              insertTryStatement({ path, name, body, t });
             }
-
-            var tryBody = path.node.body;
-            var catchBody = [];
-
-            // Separate custom catch body from try body
-            if (catchBlockIndex > -1) {
-              tryBody.body = body.slice(0, catchBlockIndex);
-              catchBody = body.slice(catchBlockIndex);
-
-              // Remove @onError leading comment from catch body
-              delete catchBody[0].leadingComments;
-
-              // Remove trailing comments from preceeding block
-              if (catchBlockIndex > 0) {
-                delete tryBody.body[catchBlockIndex - 1].trailingComments;
-              }
-            }
-
-            catchBody = catchBody.concat(
-              babylon.parse(getCatchBlock(name)).program.body
-            );
-
-            path.node.body = t.blockStatement([
-              t.tryStatement(
-                tryBody,
-                t.catchClause(
-                  t.identifier('err'),
-                  t.blockStatement(
-                    catchBody
-                  )
-                )
-              )
-            ]);
           }
         });
       },
